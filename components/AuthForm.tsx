17528e324971f9ff35e5dbc6686e59c9f90caef8
Raxit Singh <raxitsingh4321@gmail.com>
2025-03-01 16:23:43 +0530

    Remove unused Full Name field in AuthForm

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -71,13 +71,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
                       Full Name
                     </FormLabel>
 
-                    <FormControl>
-                      <Input
-                        placeholder="Enter your full name"
-                        className="shad-input"
-                        {...field}
-                      />
-                    </FormControl>
+                    
                   </div>
 
                   <FormMessage className="shad-form-message" />
